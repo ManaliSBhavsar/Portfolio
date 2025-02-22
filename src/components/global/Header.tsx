24121a874d870a8a37bf40edf24b52b6053452b9
ManaliSBhavsar <manalisbhavsar@gmail.com>
2025-02-22 12:32:41 +0530

    minor design fixes

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
       animate={{ x: 0, opacity: 1 }}
       transition={{ duration: 1.2, ease: "easeOut" }}
       className='header mb-12 pt-16'>
-      <div className='flex flex-col items-start gap-[15px] md:flex-row md:gap-0 md:items-center md:justify-between'>
+      <div className='flex flex-col items-baseline gap-[15px] md:flex-row md:gap-0 md:justify-between'>
         <div className='flex gap-8 items-center'>
           <div>
             <h1 className='text-xl md:text-2xl font-bold text-gray-800 dark:text-white'>Manali Bhavsar</h1>
@@ -27,7 +27,7 @@ function Header() {
             />
           </span>
         </div>
-        <div className='flex flex-col gap-[10px] items-center'>
+        <div className='flex flex-col gap-[5px] items-center'>
           <a href='#' className='text-xs inline-flex items-center gap-2 px-3 py-2 w-fit text-white bg-gray-800 border-2 border-transparent hover:border-gray-800 hover:text-gray-800 hover:bg-white focus:border-gray-800 focus:text-gray-800 focus:bg-white transition-colors duration-300 ease-in-out rounded-lg shadow-md font-semibold transform'>
             Download Resume
             <FontAwesomeIcon icon={faDownload} />
--- src/components/global/SocialLinks.tsx
+++ src/components/global/SocialLinks.tsx
@@ -2,26 +2,24 @@ import { socialLinks } from "../../data/social";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SocialLinks() {
-    return (
-        <>
-            <div className="flex flex-row justify-center gap-x-2">
-                {socialLinks.map((link) => (
-                    <a className="flex justify-center items-center rounded-lg w-10 h-10 text-gray-800 text-lg 
-              hover:bg-gray-800 hover:text-white  hover:shadow-lg 
-              focus:bg-gray-800 focus:text-white  focus:shadow-lg 
-              transition-all duration-300 ease-in-out"
-                        key={link.id}
-                        href={link.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label="Social Media Link"
-                    >
-                        <FontAwesomeIcon icon={link.icon} />
-                    </a>
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-row justify-center gap-x-2">
+        {socialLinks.map((link) => (
+          <a
+            className="flex justify-center items-center rounded-full w-8 h-8 text-gray-800 text-lg hover:bg-gray-800 hover:text-white hover:shadow-lg focus:bg-gray-800 focus:text-white focus:shadow-lg transition-all duration-300 ease-in-out"
+            key={link.id}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Social Media Link"
+          >
+            <FontAwesomeIcon icon={link.icon} />
+          </a>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default SocialLinks
